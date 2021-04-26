44e9c28aa1f52b1cbc436a0f9691ce1769895fd3
Jon Whittinghgam <jon@crownparkcomputing.co.uk>
2021-04-26 22:58:03 +0100

    latets update

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,10 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
+  <div class="about pa-16">
+    <h1>To-Do Application about page</h1>
+    <p>Tempor eiusmod nulla do anim consequat quis eu pariatur quis.Aliqua eu excepteur nulla nisi commodo ullamco. Duis irure cillum ullamco ea esse. Cupidatat magna laborum irure pariatur adipisicing veniam irure.Elit amet exercitation duis commodo duis. Nulla et ut magna non ipsum irure voluptate exercitation esse. Culpa nulla enim Lorem ullamco ex deserunt pariatur fugiat incididunt enim ea irure. Excepteur occaecat sint ex nulla nostrud. Sit ex commodo nulla nostrud.
+Et commodo dolore non consectetur non in sunt. In dolor aute sunt ea culpa culpa officia esse. Id enim ex quis incididunt excepteur excepteur exercitation est consectetur magna qui laborum Lorem.</p>
+  <p> Ut amet veniam veniam ullamco. Id fugiat dolore nulla commodo cupidatat fugiat dolore et id ea labore.
+postrud anim nulla est elit. Consequat labore eu ipsum ex aliquip laboris sit elit amet sunt mollit pariatur. Esse nostrud fugiat cillum consequat eiusmod Lorem voluptate officia veniam esse ea eiusmod ea. Nisi esse exercitation laborum id. Voluptate ullamco elit culpa aute laborum do aute duis officia et. Elit laborum veniam et consequat culpa amet duis dolor laborum.
+at id amet Lorem labore ex duis exercitation anim consectetur eiusmod duis ad commodo consequat. Occaecat anim excepteur dolore amet deserunt amet ut veniam sit est. Officia voluptate dolor excepteur consequat reprehenderit duis culpa. Qui ipsum aliquip ad culpa ad nostrud laboris.</p>
   </div>
 </template>
